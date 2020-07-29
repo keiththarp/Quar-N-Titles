7ffb7dcca0d72dc542ef4af46ed81b55c6f8849a
Marc <mlanglois333@icloud.com>
2020-07-29 19:28:46 -0400

    added var

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,26 @@
-function testing(){
+const searchBar = document.getElementById("searchBar");
+var searchVal = searchBar.val();
 
-    var movie = "fargo";
-            var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
-    
-            
-            $.ajax({
-              url: queryURL,
-              method: "GET"
-            }).then(function(response) {console.log(response);});
-        }
-        testing();
+
+
+function search() {
+
+  var movie = searchVal;
+  var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+
+    console.log(response);
+    var title = response;
+    var genre = response;
+    var image = response;
+    var rating = response;
+
+
+  });
+}
+search();
