32ca7601891dc1aa63411ad637cc6b9e423269df
Marc <mlanglois333@icloud.com>
2020-08-03 18:20:13 -0400

    Added reformatting

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,5 @@
 var selectedMov = []
-//var displaycard=
+
 
 function search() {
 
@@ -13,53 +13,49 @@ function search() {
     method: "GET"
   }).then(function (response) {
 
-    if(response.Response==="False"){alert("No results found"); return}
-    else{
-
-    console.log(response);
-    var title = response.Title;
-    var genre = response.Genre;
-    var imageSrc = response.Poster;
-    var rating = response.Rated;
-    var movObj = { movTitle: title, movGenre: genre, movRate: rating, movImgSrc: imageSrc }
+    if (response.Response === "False") { alert("No results found"); return }
+    else {
 
 
-
-    $("#title").html(title);
-    $("#genre").html(genre);
-    $("#image").html("<img src='" + imageSrc + "'>");
-    $("#rating").html(rating);
-
-    function addBut(){
-      var selBut = $("<button>");
-      selBut.text("Add");
-      selBut.on("click", function () {
-  
-  
-        selectedMov.push(movObj);
-  
-        
-        localStorage.setItem('movies', JSON.stringify(selectedMov));
-  
-        console.log(movObj);
-  
-  
-        console.log(selectedMov);
-        console.log(localStorage.movies);
-        renderList();
-      });
-$("#image").append(selBut);
-    }
+      var title = response.Title;
+      var genre = response.Genre;
+      var imageSrc = response.Poster;
+      var rating = response.Rated;
+      var movObj = { movTitle: title, movGenre: genre, movRate: rating, movImgSrc: imageSrc }
 
 
-function exist(){
-    for (i = 0; i < selectedMov.length; i++) {
-      if (title===selectedMov[i].movTitle) {return true}
 
-    }
-  }
-  if (exist === true) {return}
-  else {addBut()}
+      $("#title").html(title);
+      $("#genre").html(genre);
+      $("#image").html("<img src='" + imageSrc + "'>");
+      $("#rating").html(rating);
+
+      function addBut() {
+        var selBut = $("<button>");
+        selBut.text("Add");
+        selBut.on("click", function () {
+
+
+          selectedMov.push(movObj);
+
+
+          localStorage.setItem('movies', JSON.stringify(selectedMov));
+
+
+          renderList();
+        });
+        $("#image").append(selBut);
+      }
+
+
+      function exist() {
+        for (i = 0; i < selectedMov.length; i++) {
+          if (title === selectedMov[i].movTitle) { return true }
+
+        }
+      }
+      if (exist === true) { return }
+      else { addBut() }
     }
   });
 };
@@ -74,23 +70,23 @@ function renderList() {
     movBut.text(selectedMov[i].movTitle);
     movBut.attr('data-index', i);
     movBut.attr('value', selectedMov[i]);
-    movBut.on("click", function () { 
-      var Index=this.dataset.index; 
-      
+    movBut.on("click", function () {
+      var Index = this.dataset.index;
+
       $("#title").html(selectedMov[Index].movTitle);
       $("#rating").html(selectedMov[Index].movRate);
       $("#genre").html(selectedMov[Index].movGenre);
-      $("#image").html("<img src='" + selectedMov[Index].movImgSrc+ "'>");
+      $("#image").html("<img src='" + selectedMov[Index].movImgSrc + "'>");
       var removeBut = $("<button>");
       removeBut.text("Remove");
-      removeBut.on("click", function(){
-var thisMov=localStorage.getItem("movies");
-var movArray=JSON.parse(thisMov);
-movArray.splice(Index, 1);
-localStorage.setItem("movies", JSON.stringify( movArray));
-selectedMov.splice(Index,1);
+      removeBut.on("click", function () {
+        var thisMov = localStorage.getItem("movies");
+        var movArray = JSON.parse(thisMov);
+        movArray.splice(Index, 1);
+        localStorage.setItem("movies", JSON.stringify(movArray));
+        selectedMov.splice(Index, 1);
         clear();
-       load();
+        load();
 
       })
       $("#image").append(removeBut);
@@ -103,24 +99,24 @@ selectedMov.splice(Index,1);
 
 
 function load() {
-  console.log(localStorage.movies.length);
+
   if (localStorage.getItem("movies") === null) { return }
   else {
     var tempMov = localStorage.getItem("movies");
     selectedMov = JSON.parse(tempMov);
   }
-  console.log(selectedMov);
+
   renderList();
 }
 
-function clear(){
+function clear() {
   $("#title").html("");
   $("#rating").html("");
   $("#genre").html("");
   $("#image").html("");
 }
 
-console.log(localStorage.getItem('movies'));
+
 
 load();
 
