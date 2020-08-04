8e4a4695ac36cbf186bd92d7b66c82d7654935d0
Keith Tharp <keithstharp@gmail.com>
2020-08-04 09:27:26 -0400

    Merged with Marc's work

--- Assets/fe-script.js
+++ Assets/fe-script.js
@@ -53,30 +53,6 @@ $(document).ready(function () {
       // Building the API query URL
       const queryURL = `https://www.omdbapi.com/?t=${searchTerm}&apikey=trilogy`;
 
-<<<<<<< HEAD
-      // Calling the API
-      $.get(queryURL, function (result) {
-
-        // Destructuring the result
-        const title = result.Title;
-        const genre = result.Genre;
-        const imgSrc = result.Poster;
-        const rating = result.Rated;
-        const plot = result.Plot;
-        const reviews = result.Ratings[0].Value;
-
-        // Cloning the template
-        const thisHolder = titleBox.contents().clone().attr("id", searchTerm); // Adding id = to the search term
-        // Placing the Values in the new element
-        thisHolder.find(".title-img").css("background-image", "url(" + imgSrc + ")");
-        thisHolder.find(".title-title").text(title);
-        thisHolder.find(".title-genre").text(genre);
-        thisHolder.find(".title-rating").text(rating);
-        thisHolder.find(".title-review").text(reviews);
-        thisHolder.find(".title-plot").text(plot);// This is set to only display on >= medium screens
-        // Positioning it in the DOM
-        movieHolder.prepend(thisHolder);
-=======
       var selectedMov = []
 
 
@@ -85,126 +61,125 @@ $(document).ready(function () {
 
 
 
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function (response) {
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function (response) {
 
-    if (response.Response === "False") { alert("No results found"); return }
-    else {
-
-
-      const title = response.Title;
-      const genre = response.Genre;
-      const imgSrc = response.Poster;
-      const rating = response.Rated;
-      const plot = response.Plot;
-      const reviews = response.Ratings[0].Value;
-      var movObj = { movPlot: plot, movReviews: reviews, movTitle: title, movGenre: genre, movRate: rating, movImgSrc: imgSrc }
+        if (response.Response === "False") { alert("No results found"); return }
+        else {
 
 
-           thisHolder.find(".title-img").css("background-image", "url(" + imgSrc + ")");
-           thisHolder.find(".title-title").text(title);
-           thisHolder.find(".title-genre").text(genre);
-           thisHolder.find(".title-rating").text(rating);
-           thisHolder.find(".title-review").text(reviews);
-           thisHolder.find(".title-plot").text(plot);
-
-      function addBut() {
-        var selBut = $("<button>");
-        selBut.text("Add");
-        selBut.on("click", function () {
+          const title = response.Title;
+          const genre = response.Genre;
+          const imgSrc = response.Poster;
+          const rating = response.Rated;
+          const plot = response.Plot;
+          const reviews = response.Ratings[0].Value;
+          var movObj = { movPlot: plot, movReviews: reviews, movTitle: title, movGenre: genre, movRate: rating, movImgSrc: imgSrc }
 
 
-          selectedMov.push(movObj);
+          thisHolder.find(".title-img").css("background-image", "url(" + imgSrc + ")");
+          thisHolder.find(".title-title").text(title);
+          thisHolder.find(".title-genre").text(genre);
+          thisHolder.find(".title-rating").text(rating);
+          thisHolder.find(".title-review").text(reviews);
+          thisHolder.find(".title-plot").text(plot);
+
+          function addBut() {
+            var selBut = $("<button>");
+            selBut.text("Add");
+            selBut.on("click", function () {
 
 
-          localStorage.setItem('movies', JSON.stringify(selectedMov));
+              selectedMov.push(movObj);
 
 
-          renderList();
-        });
-        $("#newDiv").append(selBut);
-      }
+              localStorage.setItem('movies', JSON.stringify(selectedMov));
 
 
-      function exist() {
+              renderList();
+            });
+            $("#newDiv").append(selBut);
+          }
+
+
+          function exist() {
+            for (i = 0; i < selectedMov.length; i++) {
+              if (title === selectedMov[i].movTitle) { return true }
+
+            }
+          }
+          if (exist === true) { return }
+          else { addBut() }
+        }
+      });
+
+
+
+
+      function renderList() {
+        $("#newDiv").html("");
         for (i = 0; i < selectedMov.length; i++) {
-          if (title === selectedMov[i].movTitle) { return true }
+          var movBut = $("<button>");
+          movBut.text(selectedMov[i].movTitle);
+          movBut.attr('data-index', i);
+          movBut.attr('value', selectedMov[i]);
+          movBut.on("click", function () {
+            var Index = this.dataset.index;
+
+
+            thisHolder.find(".title-img").css("background-image", "url(" + selectedMov[Index].movImgSrc + ")");
+            thisHolder.find(".title-title").text(selectedMov[Index].movTitle);
+            thisHolder.find(".title-genre").text(selectedMov[Index].movGenre);
+            thisHolder.find(".title-rating").text(selectedMov[Index].movRate);
+            thisHolder.find(".title-review").text(selectedMov[Index].movReviews);
+            thisHolder.find(".title-plot").text(selectedMov[Index].movPlot);
+            var removeBut = $("<button>");
+            removeBut.text("Remove");
+            removeBut.on("click", function () {
+              var thisMov = localStorage.getItem("movies");
+              var movArray = JSON.parse(thisMov);
+              movArray.splice(Index, 1);
+              localStorage.setItem("movies", JSON.stringify(movArray));
+              selectedMov.splice(Index, 1);
+              clear();
+              load();
+
+            })
+            $("#newDiv").append(removeBut);
+
+          });
+          $("#newDiv").append(movBut);
 
         }
       }
-      if (exist === true) { return }
-      else { addBut() }
-    }
-  });
+
+
+      function load() {
+
+        if (localStorage.getItem("movies") === null) { return }
+        else {
+          var tempMov = localStorage.getItem("movies");
+          selectedMov = JSON.parse(tempMov);
+        }
+
+        renderList();
+      }
+
+      function clear() {
+        thisHolder.find(".title-img").html("");
+        thisHolder.find(".title-title").text("");
+        thisHolder.find(".title-genre").text("");
+        thisHolder.find(".title-rating").text("");
+        thisHolder.find(".title-review").text("");
+        thisHolder.find(".title-plot").text("");
+
+      }
 
 
 
-
-function renderList() {
-  $("#newDiv").html("");
-  for (i = 0; i < selectedMov.length; i++) {
-    var movBut = $("<button>");
-    movBut.text(selectedMov[i].movTitle);
-    movBut.attr('data-index', i);
-    movBut.attr('value', selectedMov[i]);
-    movBut.on("click", function () {
-      var Index = this.dataset.index;
-
-
-      thisHolder.find(".title-img").css("background-image", "url(" + selectedMov[Index].movImgSrc + ")");
-      thisHolder.find(".title-title").text(selectedMov[Index].movTitle);
-      thisHolder.find(".title-genre").text(selectedMov[Index].movGenre);
-      thisHolder.find(".title-rating").text(selectedMov[Index].movRate);
-      thisHolder.find(".title-review").text(selectedMov[Index].movReviews);
-      thisHolder.find(".title-plot").text(selectedMov[Index].movPlot);
-      var removeBut = $("<button>");
-      removeBut.text("Remove");
-      removeBut.on("click", function () {
-        var thisMov = localStorage.getItem("movies");
-        var movArray = JSON.parse(thisMov);
-        movArray.splice(Index, 1);
-        localStorage.setItem("movies", JSON.stringify(movArray));
-        selectedMov.splice(Index, 1);
-        clear();
-        load();
-
->>>>>>> 6e003e6595d910f6ef7e093a8233dd7da392d6bd
-      })
-      $("#newDiv").append(removeBut);
-
-    });
-    $("#newDiv").append(movBut);
-
-  }
-}
-
-
-function load() {
-
-  if (localStorage.getItem("movies") === null) { return }
-  else {
-    var tempMov = localStorage.getItem("movies");
-    selectedMov = JSON.parse(tempMov);
-  }
-
-  renderList();
-}
-
-function clear() {
-  thisHolder.find(".title-img").html("");
-  thisHolder.find(".title-title").text("");
-  thisHolder.find(".title-genre").text("");
-  thisHolder.find(".title-rating").text("");
-  thisHolder.find(".title-review").text("");
-  thisHolder.find(".title-plot").text("");
-
-}
-
-
-
-load();
+      load();
 
 
     },
